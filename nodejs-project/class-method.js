f4034c0d4ef62ce8234ebbff59ab8fa72e82c2db
Kerri Shotts <kerrishotts@gmail.com>
2017-08-27 16:06:45 -0400

    Create class-method.js

---
+++ nodejs-project/class-method.js
@@ -0,0 +1,19 @@
+class Rect {
+  constructor(w, h) {
+    this.w = w;
+    this.h = h;
+  }
+
+  // create a method named calculateArea that returns the area of the rectangle.
+  // if you don't remember the formula for area, it's width multipled by height.
+
+  // also, create a method named calculatePerimeter that returns the perimeter
+  // of the rectangle. If you don't remember the formula, it's twice the height
+  // and twice the width.
+}
+
+// { autofold
+module.exports = {
+  Rect
+};
+// }
