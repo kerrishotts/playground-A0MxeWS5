b22d6bb20f5b11849b4a78c07ce026e436e668ba
Kerri Shotts <kerrishotts@gmail.com>
2017-08-27 15:56:00 -0400

    Update and rename universe.spec.js to simple-point.spec.js

---
+++ nodejs-project/simple-point.spec.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var Point = require('./simple-point.js');
+
+it('set instance properties correctly', function () {
+  try {
+    const x = 79, y = 144;
+    const point = new Point(x, y);
+    assert.equal(point.x, x);
+    assert.equal(point.y, y);
+  } catch (error) {
+    printMessage('Hint 💡', 'Did you forget to set properties on \'this\', or forget to specify parameters? 🤔');
+    throw error;
+  }
+});
+
+function printMessage(channel, message) {
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
