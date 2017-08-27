556bf506fced5142b49079fbe9c9c6cc27bfd545
Kerri Shotts <kerrishotts@gmail.com>
2017-08-27 15:53:02 -0400

    Update and rename universe.js to simple-point.js

---
+++ nodejs-project/simple-point.js
@@ -0,0 +1,14 @@
+class Point {
+  constructor() {
+    // fix the constructor such that the constructor accepts an x and y
+    // argument and sets instance properties `this.x` and `this.y` to those
+    // arguments.
+    // for example, `new Point(4, 10)` should result in `this.x = 4` and `this.y = 10`.
+  }
+}
+
+// { autofold
+module.exports = {
+  Point
+};
+// }
